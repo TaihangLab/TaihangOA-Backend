261d44bf9948b4ced711b60c730b33ffa9b0c575
bailingnan <lingnanbai@gmail.com>
2024-07-23 17:00:27 +0800

    feat:IpOss类定义

---
+++ ruoyi-modules/ruoyi-project/src/main/java/org/dromara/project/domain/IpOss.java
@@ -0,0 +1,64 @@
+package org.dromara.project.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import java.io.Serializable;
+
+/**
+ * 知识产权和存储对象关联表
+ *
+ * @author bailingnan
+ * @TableName ip_oss
+ * @date 2024/02/06
+ */
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@TableName(value = "ip_oss")
+public class IpOss implements Serializable {
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+
+    @TableId(type = IdType.AUTO)
+    private Long id;
+    /**
+     * 知识产权id
+     */
+    private Long ipId;
+
+    /**
+     * 存储对象id
+     */
+    private Long ossId;
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(ipId).append(ossId).toHashCode();
+    }
+
+    /**
+     * id
+     */
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        IpOss that = (IpOss)o;
+        return new EqualsBuilder().append(ipId, that.ipId).append(ossId, that.ossId).isEquals();
+    }
+}
---
+++ ruoyi-modules/ruoyi-project/src/main/java/org/dromara/project/mapper/IpOssMapper.java
@@ -0,0 +1,22 @@
+package org.dromara.project.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.dromara.common.mybatis.core.mapper.BaseMapperPlus;
+import org.dromara.project.domain.IpOss;
+
+/**
+ * 针对表【ip_oss(知识产权和存储对象关联表)】的数据库操作Mapper
+ *
+ * @author bailingnan
+ * @createDate 2024-01-05 17:04:03
+ * @Entity com.ruoyi.ip.domain.IpOss
+ * @date 2024/02/06
+ */
+@Mapper
+public interface IpOssMapper extends BaseMapperPlus<IpOss, IpOss> {
+
+}
+
+
+
+
