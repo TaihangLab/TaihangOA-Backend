32ff436118083bd8f81d11a173948d8b4b3374c9
bailingnan <lingnanbai@gmail.com>
2024-07-24 09:56:23 +0800

    feat:ProjectAttachment类定义

---
+++ ruoyi-modules/ruoyi-project/src/main/java/org/dromara/project/domain/ProjectAttachment.java
@@ -0,0 +1,32 @@
+package org.dromara.project.domain;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 项目附件表
+ *
+ * @author bailingnan
+ * @date 2024/02/06
+ */
+@TableName(value = "project_attachment")
+@Data
+public class ProjectAttachment implements Serializable {
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+    /**
+     *
+     */
+    private Long id;
+    /**
+     *
+     */
+    private Long projectId;
+    /**
+     *
+     */
+    private Long ossId;
+}
---
+++ ruoyi-modules/ruoyi-project/src/main/java/org/dromara/project/mapper/ProjectAttachmentMapper.java
@@ -0,0 +1,19 @@
+package org.dromara.project.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.dromara.common.mybatis.core.mapper.BaseMapperPlus;
+import org.dromara.project.domain.ProjectAttachment;
+
+/**
+ * 项目附件表 数据层
+ *
+ * @author fanjiaxing
+ */
+@Mapper
+public interface ProjectAttachmentMapper extends BaseMapperPlus<ProjectAttachment, ProjectAttachment> {
+
+}
+
+
+
+
