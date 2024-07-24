bd17450b0451aefcac5b34af68ba1a6006779a06
bailingnan <lingnanbai@gmail.com>
2024-07-24 15:07:12 +0800

    feat:ProjectMilestoneOss类定义

---
+++ ruoyi-modules/ruoyi-project/src/main/java/org/dromara/project/domain/ProjectMilestoneOss.java
@@ -0,0 +1,27 @@
+package org.dromara.project.domain;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+/**
+ * 项目大事纪文件oss关联 project_milestone_oss
+ *
+ * @author bailingnan
+ * @date 2024/02/06
+ */
+@Data
+@TableName("project_milestone_oss")
+public class ProjectMilestoneOss {
+    /**
+     * 项目大事纪ID
+     */
+    @TableId
+    private Long milestoneId;
+
+    /**
+     * ossID
+     */
+
+    private Long ossId;
+}
---
+++ ruoyi-modules/ruoyi-project/src/main/java/org/dromara/project/mapper/ProjectMilestoneOssMapper.java
@@ -0,0 +1,18 @@
+package org.dromara.project.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.dromara.common.mybatis.core.mapper.BaseMapperPlus;
+import org.dromara.project.domain.ProjectMilestoneOss;
+
+/**
+ * 项目大事记与文件对应表 数据层
+ *
+ * @author bailingnan
+ */
+@Mapper
+public interface ProjectMilestoneOssMapper extends BaseMapperPlus<ProjectMilestoneOss, ProjectMilestoneOss> {
+}
+
+
+
+
