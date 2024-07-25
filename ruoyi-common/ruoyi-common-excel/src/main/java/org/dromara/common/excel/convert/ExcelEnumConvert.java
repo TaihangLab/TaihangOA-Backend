86aa8cea46413928f047d61a5b796bf385f9895b
bailingnan <lingnanbai@gmail.com>
2024-07-25 17:18:05 +0800

    feat:ExcelEnumConvert枚举格式化转换处理类

--- ruoyi-common/ruoyi-common-excel/src/main/java/org/dromara/common/excel/convert/ExcelEnumConvert.java
+++ ruoyi-common/ruoyi-common-excel/src/main/java/org/dromara/common/excel/convert/ExcelEnumConvert.java
@@ -1,17 +1,16 @@
 package org.dromara.common.excel.convert;
 
 import cn.hutool.core.annotation.AnnotationUtil;
-import cn.hutool.core.convert.Convert;
-import cn.hutool.core.util.ObjectUtil;
 import com.alibaba.excel.converters.Converter;
 import com.alibaba.excel.enums.CellDataTypeEnum;
 import com.alibaba.excel.metadata.GlobalConfiguration;
 import com.alibaba.excel.metadata.data.ReadCellData;
 import com.alibaba.excel.metadata.data.WriteCellData;
 import com.alibaba.excel.metadata.property.ExcelContentProperty;
+import lombok.extern.slf4j.Slf4j;
+import org.dromara.common.core.utils.EnumCacheUtils;
 import org.dromara.common.core.utils.reflect.ReflectUtils;
 import org.dromara.common.excel.annotation.ExcelEnumFormat;
-import lombok.extern.slf4j.Slf4j;
 
 import java.lang.reflect.Field;
 import java.util.HashMap;
@@ -32,41 +31,50 @@ public class ExcelEnumConvert implements Converter<Object> {
 
     @Override
     public CellDataTypeEnum supportExcelTypeKey() {
-        return null;
+        return CellDataTypeEnum.STRING;
     }
 
     @Override
-    public Object convertToJavaData(ReadCellData<?> cellData, ExcelContentProperty contentProperty, GlobalConfiguration globalConfiguration) {
-        cellData.checkEmpty();
-        // Excel中填入的是枚举中指定的描述
-        Object textValue = switch (cellData.getType()) {
-            case STRING, DIRECT_STRING, RICH_TEXT_STRING -> cellData.getStringValue();
-            case NUMBER -> cellData.getNumberValue();
-            case BOOLEAN -> cellData.getBooleanValue();
-            default -> throw new IllegalArgumentException("单元格类型异常!");
-        };
-        // 如果是空值
-        if (ObjectUtil.isNull(textValue)) {
-            return null;
-        }
-        Map<Object, String> enumCodeToTextMap = beforeConvert(contentProperty);
-        // 从Java输出至Excel是code转text
-        // 因此从Excel转Java应该将text与code对调
-        Map<Object, Object> enumTextToCodeMap = new HashMap<>();
-        enumCodeToTextMap.forEach((key, value) -> enumTextToCodeMap.put(value, key));
-        // 应该从text -> code中查找
-        Object codeValue = enumTextToCodeMap.get(textValue);
-        return Convert.convert(contentProperty.getField().getType(), codeValue);
+    public Object convertToJavaData(ReadCellData<?> cellData, ExcelContentProperty contentProperty,
+        GlobalConfiguration globalConfiguration) {
+        //        Object codeValue = cellData.getData();
+        //        // 如果是空值
+        //        if (ObjectUtil.isNull(codeValue)) {
+        //            return null;
+        //        }
+        //        Map<Object, String> enumValueMap = beforeConvert(contentProperty);
+        //        String textValue = enumValueMap.get(codeValue);
+        //        return Convert.convert(contentProperty.getField().getType(), textValue);
+
+        String description = cellData.getStringValue();
+        log.info("description:{}", description);
+        ExcelEnumFormat anno = getAnnotation(contentProperty.getField());
+        return EnumCacheUtils.findByValue(anno.enumClass(), description, null);
     }
 
     @Override
-    public WriteCellData<String> convertToExcelData(Object object, ExcelContentProperty contentProperty, GlobalConfiguration globalConfiguration) {
-        if (ObjectUtil.isNull(object)) {
+    public WriteCellData<String> convertToExcelData(Object object, ExcelContentProperty contentProperty,
+        GlobalConfiguration globalConfiguration) {
+        //        if (ObjectUtil.isNull(object)) {
+        //            return new WriteCellData<>("");
+        //        }
+        //        Map<Object, String> enumValueMap = beforeConvert(contentProperty);
+        //        String value = Convert.toStr(enumValueMap.get(object), "");
+        //        return new WriteCellData<>(value);
+        //        return new WriteCellData<>(object.toString());
+        if (object == null) {
             return new WriteCellData<>("");
         }
+
+        ExcelEnumFormat anno = getAnnotation(contentProperty.getField());
         Map<Object, String> enumValueMap = beforeConvert(contentProperty);
-        String value = Convert.toStr(enumValueMap.get(object), "");
-        return new WriteCellData<>(value);
+
+        String description = enumValueMap.get(getEnumValue(object, anno.codeField()));
+        return new WriteCellData<>(description);
+    }
+
+    private Object getEnumValue(Object enumInstance, String codeField) {
+        return ReflectUtils.invokeGetter(enumInstance, codeField);
     }
 
     private Map<Object, String> beforeConvert(ExcelContentProperty contentProperty) {
